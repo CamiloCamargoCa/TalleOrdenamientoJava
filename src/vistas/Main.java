448b9ce6ddcd8df97420106583567317aa5f4339
mcastilloa <desarrolloweb4@tae-ltda.com>
2022-03-15 08:14:56 -0500

    añade form para seleccionar archivo

--- src/vistas/Main.java
+++ src/vistas/Main.java
@@ -8,7 +8,6 @@ import controladores.OrdernarNumeros;
 import controladores.LeerTxt;
 import controladores.CrearTxt;
 import java.io.IOException;
-import java.util.Arrays;
 /**
  *
  * @author desar
@@ -16,35 +15,8 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) throws IOException {
         
-        //array inicializado null, el que toma arraglos de la lectura txt y pasa la data a la clase de ordenamiento de numero
-        int arrayData[];
-        
-        //array con numeros ya ordenados
-        int arrayDataNew[];
-        
-        //url de documento txt
-        final String URL = "C:\\Users\\desar\\OneDrive\\Escritorio\\uu2022_1\\PruebasVerSoftware\\Corte1\\Numeros.txt";
-        
-        //url de documento txt
-        final String ARCHIVO_NUEVO = "ArchivoFinal.txt";
-        
-        //objeto clase ordenar numro
-        OrdernarNumeros on = new OrdernarNumeros();
-        
-        //objeto clase leer txt
-        LeerTxt lt = new LeerTxt();
-        
-       //objeto clase crear txt
-        CrearTxt ct = new CrearTxt();
-        
-        //datos en array extraidos del txt se asigna a variable arrayData
-        arrayData = lt.extraerData(URL);
-        
-        //función que ordena array de numeros y se asignan a arrayDataNew
-        arrayDataNew = on.organizarMenorMayor(arrayData);
-        
-        //funcion para generar archivo nuevo
-        ct.crearTxtNuevo(arrayDataNew, ARCHIVO_NUEVO);
+        formLeer ven = new formLeer();
+        ven.setVisible(true);
         
     }
 }
